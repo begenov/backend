8bea69e531a2dd4f98457f4b52f25f296d55a502
begenov <begenovorazali@icloud.com>
2023-05-15 10:48:11 +0600

    add store structure and newstore function

--- db/sqlc/storage.go
+++ db/sqlc/storage.go
@@ -1,1 +1,17 @@
 package db
+
+import (
+	"database/sql"
+)
+
+type Store struct {
+	*Queries
+	db *sql.DB
+}
+
+func NewStore(db *sql.DB) *Store {
+	return &Store{
+		db:      db,
+		Queries: New(db),
+	}
+}
