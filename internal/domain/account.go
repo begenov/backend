30f426615f7758aa4704171767613f9f4910eca3
begenov <begenovorazali@icloud.com>
2023-06-15 17:42:52 +0600

    init account domain

---
+++ internal/domain/account.go
@@ -0,0 +1,27 @@
+package domain
+
+import "time"
+
+type Account struct {
+	ID        int       `json:"id"`
+	Owner     string    `json:"owner"`
+	Balance   int       `json:"balance"`
+	Currency  string    `json:"currency"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+type CreateAccountParams struct {
+	Owner    string `json:"owner"`
+	Balance  int    `json:"balance"`
+	Currency string `json:"currency"`
+}
+
+type ListAccountsParams struct {
+	Limit  int `json:"limit"`
+	Offset int `json:"offset"`
+}
+
+type UpdateAccountParams struct {
+	ID      int `json:"id"`
+	Balance int `json:"balance"`
+}
