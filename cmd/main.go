bbcbb5a7a98e00656e841c184ee9fecdc1281790
begenov <begenovorazali@icloud.com>
2023-06-17 20:02:29 +0600

    update

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"log"
+
+	"github.com/begenov/backend/internal/app"
+	"github.com/begenov/backend/internal/config"
+)
+
+func main() {
+	cfg, err := config.Init(".")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	if err := app.Run(cfg); err != nil {
+		log.Fatalf("[ERROR] Run Application: %v\n", err)
+	}
+}
---
+++ pkg/db/connection.go
@@ -0,0 +1,20 @@
+package db
+
+import (
+	"database/sql"
+
+	_ "github.com/lib/pq"
+)
+
+func NewDB(driver, dsn string) (*sql.DB, error) {
+	db, err := sql.Open(driver, dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
