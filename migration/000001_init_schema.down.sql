920274a95cb40a08d83c397e024735e8a3ab82f1
begenov <begenovorazali@icloud.com>
2023-06-18 20:38:19 +0600

    add user

--- migration/000001_init_schema.down.sql
+++ migration/000001_init_schema.down.sql
@@ -3,3 +3,5 @@ DROP TABLE IF EXISTS entries;
 DROP TABLE IF EXISTS transfers;
 
 DROP TABLE IF EXISTS accounts;
+
+DROP TABLE IF EXISTS users;
--- migration/000001_init_schema.up.sql
+++ migration/000001_init_schema.up.sql
@@ -21,6 +21,19 @@ CREATE TABLE "transfers" (
   "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
+CREATE TABLE "users" (
+  "username" varchar PRIMARY KEY,
+  "hashed_password" varchar NOT NULL,
+  "full_name" varchar NOT NULL,
+  "email" varchar UNIQUE NOT NULL,
+  "password_changed_at" timestamptz NOT NULL DEFAULT '0001-01-01 00:00:00Z',
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+ALTER TABLE "accounts" ADD FOREIGN KEY ("owner") REFERENCES "users" ("username");
+
+CREATE UNIQUE INDEX ON "accounts" ("owner", "currency");
+
 ALTER TABLE "entries" ADD FOREIGN KEY ("account_id") REFERENCES "accounts" ("id");
 
 ALTER TABLE "transfers" ADD FOREIGN KEY ("from_account_id") REFERENCES "accounts" ("id");
